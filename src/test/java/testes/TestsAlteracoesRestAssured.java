edc0cad6859ab0613f32990ae435c4dff1283d24
Marilia Alves <marilia.borges01@outlook.com>
2023-03-02 17:55:10 -0300

    Alterações no método GET para ficar o mais genérico possível

--- src/test/java/testes/TestsAlteracoesRestAssured.java
+++ src/test/java/testes/TestsAlteracoesRestAssured.java
@@ -1,13 +1,24 @@
 package testes;
 
 import base.BaseTests;
+import io.restassured.http.ContentType;
 import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+import utils.PropertiesUtils;
 
 public class TestsAlteracoesRestAssured extends BaseTests {
+    PropertiesUtils prop = new PropertiesUtils();
+
+    @BeforeSuite
+    public void configClasse()
+    {
+        //Inicializar variáveis também
+        super.uri = prop.getProp("base_url") + "/data/2.5/weather";
+    }
 
     @Test
-    public void testeMethodo()
+    public void testeMethod()
     {
         super.params.put("q","Fortaleza,BR");
         super.params.put("appid", "6a6424d1582a9636d6d186ea0cc53620");
@@ -15,5 +26,18 @@ public class TestsAlteracoesRestAssured extends BaseTests {
 
         super.GET();
         Assert.assertEquals(response.getStatusCode(),400);
+    }
+
+    @Test
+    public void testeSucesso()
+    {
+        super.params.put("q","Fortaleza,BR");
+        super.params.put("appid", "6a6424d1582a9636d6d186ea0cc53620");
+        super.params.put("units","metric");
+
+        super.request.contentType(ContentType.JSON);
+
+        super.GET();
+        Assert.assertEquals(response.getStatusCode(),200);
     }
 }
